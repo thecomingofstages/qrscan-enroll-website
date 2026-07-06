727540d4c9c753d44553da7f4d2c438cd567a82f
it_tcos <it_tcos@ittcos.tail5c7dbd.ts.net>
2026-07-06 09:37:33 +0000

    modified for local machine deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+	root: path.join(__dirname),
+  },
+  allowedDevOrigins: ['100.104.99.50'],
 };
 
 export default nextConfig;
